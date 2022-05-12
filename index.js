36e20f0f6ee3e071c8d27581eca89b1881afd201
Jose <jmgarcia0202@gmail.com>
2022-05-11 19:39:10 -0700

    redid

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function divide() {
+const divide = (a = 2000,b = 100) =>  a/b
+const square = a => a*a
+const add = (a,b) => a+b
+
+
+/*function divide() {
     return 2000/100;
 }
-
 const square = (x) => x*x;
-
-const add = (par1, par2) => par1+par2;
+const add = (par1, par2) => par1+par2;*/
